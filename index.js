69930fb7e31d6aac03dfdf1cbb3e520e9ee9f150
Daniel <contato.daniel.dev@gmail.com>
2022-11-24 18:12:22 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const getName = () => {
+  return "Daniel";
+};
+
+const getAge = () => {
+  return 26;
+};
+
+const getProfission = () => {
+  return "Desenvolvedor de aplicativos";
+};
+
+console.log(getName());
+console.log(getAge());
+console.log(getProfission());
